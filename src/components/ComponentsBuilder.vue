ccd3293f371ba6c2c8550763593691563f0c19df
jsdv <841207@gmail.com>
2018-01-29 19:43:42 +0800

    init

---
+++ src/components/ComponentsBuilder.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class='componentsBuilder'>
+      <el-switch />
+  </div>
+</template>
+
+<script>
+const data = {
+  valueType: 'string',
+  valueTypes: ['string', 'number', 'enum', 'boolean'],
+  componentType: 'input',
+  componentTypeEnum: ['input', 'switch'],
+  name: '',
+  default: ''
+}
+export default {
+  name: 'ComponentsBuilder',
+  data () {
+    return data
+  },
+  created () {},
+  props: {
+    type: {
+      type: [String, Boolean, Array, Number],
+      default: false
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
